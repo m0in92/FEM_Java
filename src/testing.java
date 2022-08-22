654642e46f6428269cf3e5774778c11fc3f61c4d
unknown <moinahmed100@gmail.com>
2022-08-22 19:00:32 -0400

    ammended

--- src/testing.java
+++ src/testing.java
@@ -3,19 +3,19 @@ import model.*;
 import util.*;
 import java.io.PrintWriter;
 import java.io.FileWriter;
-import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
 
 
 public class testing {
     public static void main (String[] args) throws Exception {
+        // test model.Dof
         Dof Dof_test = new Dof(2, 36.1);
-
         out.println(Dof_test.value);
 
-        FileWriter testFile = new FileWriter("test.txt");
-        PrintWriter pr1 = new PrintWriter(testFile);
-        UTIL.printDate(pr1);
-
-        out.println(UTIL.direction("xc"));
+        // test util.UTIL
+        var pr1 = new FePrintWriter();
+        UTIL.printDate(pr1.getPrinter("test.txt")); // checks calendar write to test.txt
+        out.println(UTIL.direction("xc")); // checks directions method
     }
 }
---
+++ src/util/FePrintWriter.java
@@ -0,0 +1,17 @@
+package util;
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+
+public class FePrintWriter {
+    PrintWriter PR;
+    public PrintWriter getPrinter(String fileOut) {
+        try {
+            PR = new PrintWriter(new BufferedWriter(new FileWriter(fileOut)));
+        } catch (Exception e) {
+            UTIL.errorMsg("cannot open output file: " + fileOut);
+        }
+        return PR;
+    }
+}
